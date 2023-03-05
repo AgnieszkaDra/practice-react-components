21ed6d367193ec27ec2e26f5d367002d554cbdee
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-03-05 09:22:02 +0100

    end task 01

--- 01/app.js
+++ 01/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.querySelector('#root'));
@@ -7,10 +7,29 @@ class App extends React.Component {
     state = {
         counter: 0,
     }
+    
     render() {
         console.log('render');
-
         return <h1>{ this.state.counter }</h1>
+    }
+
+    myCallback() {
+        const {counter} = this.state;
+        this.setState({counter: counter + 1})
+    }
+
+    componentDidMount() {
+        console.log('Komponent został zamontowany!'); 
+        this.id = setInterval(this.myCallback.bind(this), 5000)  
+     }
+
+     componentDidUpdate(){
+        console.log('Component did update!')   
+    }
+
+    componentWillUnmount(){
+        console.log('Component will unmount');
+        clearInterval(this.id);
     }
 }
 
